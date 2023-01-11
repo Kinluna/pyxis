e5075bfa13c007b5680bc3585f2cda8e9230a2fb
Kinluna <lucapitzus@gmail.com>
2023-01-11 16:55:12 +0100

    Update faces2.1.py

--- faces2.1.py
+++ faces2.1.py
@@ -8,29 +8,6 @@ import shutil
 from scipy.spatial import distance as dist
 
 
-def get_smile_tl(top_lip):
-    # this function check if the borders of the mouth are higher than the center
-    return top_lip[0][1] < top_lip[4][1] or \
-           top_lip[6][1] < top_lip[2][1] or \
-           top_lip[0][1] < top_lip[6][1] or \
-           top_lip[6][1] < top_lip[0][1]
-
-
-def get_smile_op(top_lip, bottom_lip):
-    # this function calculate the aspect ratio of the mouth
-    # Pitzus - aggiunto check per apertura bocca
-    inner_mouth_distance = dist.euclidean(bottom_lip[9], top_lip[9])
-    outer_mouth_distance = dist.euclidean(bottom_lip[2], top_lip[2])
-
-    if inner_mouth_distance != 0:
-        mouth_distance = inner_mouth_distance
-    else:
-        mouth_distance = outer_mouth_distance
-
-    sar = dist.euclidean(top_lip[0], top_lip[6]) / mouth_distance
-    return 3 < sar < 5.5
-
-
 def get_eye(eye):
     # this functon calculate the aspect ratio of the given eye
     A = dist.euclidean(eye[1], eye[5])
@@ -42,24 +19,7 @@ def get_eye(eye):
     return eye
 
 
-def smile_check(res_image):
-    face_landmarks_list = face_recognition.face_landmarks(res_image)
-    for face_landmark in face_landmarks_list:
-        # gets lips
-        toplip = face_landmark["top_lip"]
-        bottomlip = face_landmark["bottom_lip"]
-        # check if the subject is smiling
-        smiletl = get_smile_tl(toplip)
-        smileop = get_smile_op(toplip, bottomlip)
-        sorriso = smiletl or smileop
-        # compare with estimated threshold
-        # gets the time fot the facial expr. wink
-        if sorriso:
-            return True
-        return False
-
-
-def specular_right_wink_check(res_image):
+def wink_check(res_image):
     face_landmarks_list = face_recognition.face_landmarks(res_image)
     for face_landmark in face_landmarks_list:
         # get the eyes
@@ -68,47 +28,35 @@ def specular_right_wink_check(res_image):
         # calculate the aspect ratio of the eyes
         apertura_sx = get_eye(occhiosx)
         apertura_dx = get_eye(occhiodx)
+        print(f"Occhio sx: {apertura_sx}")
+        print(f"Occhio dx: {apertura_dx}")
         # compare with estimated threshold
-        occhiolino = ((apertura_sx < 0.23) and (apertura_dx >= 0.25))
-        # gets the time fot the facial expr. wink
-        if occhiolino:
-            return True
-        return False
+        # specular left wink
+        if (apertura_sx >= 0.24) and (apertura_dx < 0.23):
+            return 1
+        # specular right wink
+        elif (apertura_sx < 0.23) and (apertura_dx >= 0.24):
+            return 2
+        else:
+            return 0
 
 
-def specular_left_wink_check(res_image):
-    face_landmarks_list = face_recognition.face_landmarks(res_image)
-    for face_landmark in face_landmarks_list:
-        # get the eyes
-        occhiosx = face_landmark["left_eye"]
-        occhiodx = face_landmark["right_eye"]
-        # calculate the aspect ratio of the eyes
-        apertura_sx = get_eye(occhiosx)
-        apertura_dx = get_eye(occhiodx)
-        # compare with estimated threshold
-        occhiolino = ((apertura_sx >= 0.25) and (apertura_dx < 0.23))
-        # gets the time fot the facial expr. wink
-        if occhiolino:
-            return True
-        return False
-
-
-# metodo che flagga come true se non sono ancora passati 5 minuti
-def time_flag_visitors(saved_image_name):
+# metodo che flagga come true se non sono ancora passati i minuti specificati
+def time_flag_minutes(saved_image_name, lapse_length):
     now = datetime.now()
-    now_less_five_minutes = now - timedelta(minutes=5)
+    now_less_minutes = now - timedelta(minutes=lapse_length)
     saved_image_date = datetime.strptime(saved_image_name, "%d_%m_%Y_%H_%M_%S")
-    if now_less_five_minutes >= saved_image_date:
+    if now_less_minutes >= saved_image_date:
         return False
     return True
 
 
-# metodo che flagga come true se non sono ancora passati 2 secondi
-def time_flag_unknown(saved_image_name):
+# metodo che flagga come true se non sono ancora passati i secondi specificati
+def time_flag_seconds(saved_image_name, lapse_length):
     now = datetime.now()
-    now_less_two_seconds = now - timedelta(seconds=2)
+    now_less_seconds = now - timedelta(seconds=lapse_length)
     saved_image_date = datetime.strptime(saved_image_name, "%d_%m_%Y_%H_%M_%S")
-    if now_less_two_seconds >= saved_image_date:
+    if now_less_seconds >= saved_image_date:
         return False
     return True
 
@@ -155,7 +103,7 @@ process_this_frame = True
 while True:
     ret, frame = webcam_capture.read()
     # rescale del frame a 1/5 su ogni asse
-    small_frame = cv2.resize(frame, None, fx=0.20, fy=0.20)
+    small_frame = cv2.resize(frame, None, fx=0.2, fy=0.2)
     # conversione del colore a bgr a rgb
     rgb_small_frame = cv2.cvtColor(small_frame, 4)
 
@@ -199,15 +147,18 @@ while True:
                         myfile.close()
                         # metodo di controllo
                         if len(passcheck) < len(userpass):
+                            wink = wink_check(rgb_small_frame)
                             cv2.putText(frame, f"{len(passcheck)}/{len(userpass)}", (20, 70), cv2.FONT_HERSHEY_PLAIN, 3,
                                         (255, 0, 0), 2)
-                            if (not time_flag_unknown(time_for_winks)) & specular_left_wink_check(rgb_small_frame):
-                                passcheck = passcheck + "1"
-                                label = f"{shown_name} is left winking."
-                                time_for_winks = time.strftime("%d_%m_%Y_%H_%M_%S")
-                            elif (not time_flag_unknown(time_for_winks)) & specular_right_wink_check(rgb_small_frame):
-                                label = f"{shown_name} is right winking."
-                                passcheck = passcheck + "2"
+                            if (not time_flag_seconds(time_for_winks, 2)) & (wink != 0):
+                                if wink == 1:
+                                    which_eye_is_winking = "left"
+                                elif wink == 2:
+                                    which_eye_is_winking = "right"
+                                else:
+                                    which_eye_is_winking = "not"
+                                passcheck = passcheck + f"{wink_check(rgb_small_frame)}"
+                                label = f"{shown_name} is {which_eye_is_winking} winking."
                                 time_for_winks = time.strftime("%d_%m_%Y_%H_%M_%S")
                         else:
                             if passcheck == userpass:
@@ -225,7 +176,7 @@ while True:
                     for (i, item) in enumerate(list_encoding_tup):
                         if "visitatore" in item[0]:
                             date_string = time_string_prep(item[3], item[0])
-                            if time_flag_visitors(date_string):
+                            if time_flag_minutes(date_string, 5):
                                 chrono_trigger = True
                                 break
                     if not chrono_trigger:
@@ -242,37 +193,39 @@ while True:
 
                 # Caso in cui la cam riconosce uno sconosciuto
                 elif match & (target_name == "sconosciuti"):
-                    counter = 0
-                    isExist = os.path.exists(f"dataset/visitatore{counter}")
-                    while isExist:
-                        counter = counter + 1
+                    image_name_prep = time_string_prep(image_name, "sconosciuti")
+                    if not time_flag_seconds(image_name_prep, 30):
+                        counter = 0
                         isExist = os.path.exists(f"dataset/visitatore{counter}")
-                    new_directory = f"dataset/visitatore{counter}"
-                    os.mkdir(new_directory)
-                    # Salvataggio frame E spostamento immagine da sconosciuti a visitatoreX
-                    for (i, item) in enumerate(list_encoding_tup):
-                        if item[3] == image_name:
-                            # spostamento da folder sconosciuti a visitatoreX
-                            old_name = item[3]
-                            # sostituisco la stringa sconosciuti con visitatoreX
-                            old_photo_new_name = old_name.replace('sconosciuti', f'visitatore{counter}')
-                            shutil.move(old_name, old_photo_new_name)
-                            # creazione nuova tupla
-                            old_photo_new_image = face_recognition.load_image_file(old_photo_new_name)
-                            old_photo_new_encoding = face_recognition.face_encodings(old_photo_new_image)[0]
-                            old_photo_new_encoding_tup = (f"visitatore{counter}", old_photo_new_encoding,
-                                                          old_photo_new_image, old_photo_new_name)
-                            list_encoding_tup.append(old_photo_new_encoding_tup)
-                            # rimozione vecchia tupla
-                            list_encoding_tup.pop(i)
-                            # salvataggio frame in visitatoreX
-                            snapshot = time.strftime("%d_%m_%Y_%H_%M_%S")
-                            new_photo_new_name = f"{new_directory}/{snapshot}.jpg"
-                            cv2.imwrite(new_photo_new_name, frame)
-                            target_image = face_recognition.load_image_file(new_photo_new_name)
-                            target_encoding = face_recognition.face_encodings(target_image)[0]
-                            encoding_tup = (f"visitatore{counter}", target_encoding, target_image, new_photo_new_name)
-                            list_encoding_tup.append(encoding_tup)
+                        while isExist:
+                            counter = counter + 1
+                            isExist = os.path.exists(f"dataset/visitatore{counter}")
+                        new_directory = f"dataset/visitatore{counter}"
+                        os.mkdir(new_directory)
+                        # Salvataggio frame E spostamento immagine da sconosciuti a visitatoreX
+                        for (i, item) in enumerate(list_encoding_tup):
+                            if item[3] == image_name:
+                                # spostamento da folder sconosciuti a visitatoreX
+                                old_name = item[3]
+                                # sostituisco la stringa sconosciuti con visitatoreX
+                                old_photo_new_name = old_name.replace('sconosciuti', f'visitatore{counter}')
+                                shutil.move(old_name, old_photo_new_name)
+                                # creazione nuova tupla
+                                old_photo_new_image = face_recognition.load_image_file(old_photo_new_name)
+                                old_photo_new_encoding = face_recognition.face_encodings(old_photo_new_image)[0]
+                                old_photo_new_encoding_tup = (f"visitatore{counter}", old_photo_new_encoding,
+                                                              old_photo_new_image, old_photo_new_name)
+                                list_encoding_tup.append(old_photo_new_encoding_tup)
+                                # rimozione vecchia tupla
+                                list_encoding_tup.pop(i)
+                                # salvataggio frame in visitatoreX
+                                snapshot = time.strftime("%d_%m_%Y_%H_%M_%S")
+                                new_photo_new_name = f"{new_directory}/{snapshot}.jpg"
+                                cv2.imwrite(new_photo_new_name, frame)
+                                target_image = face_recognition.load_image_file(new_photo_new_name)
+                                target_encoding = face_recognition.face_encodings(target_image)[0]
+                                encoding_tup = (f"visitatore{counter}", target_encoding, target_image, new_photo_new_name)
+                                list_encoding_tup.append(encoding_tup)
                     find_a_person = True
 
                 # Caso in cui la cam riconosce un nuovo sconosciuto
@@ -281,7 +234,7 @@ while True:
                     for (i, item) in enumerate(list_encoding_tup):
                         if "sconosciuti" in item[0]:
                             date_string = time_string_prep(item[3], item[0])
-                            if time_flag_visitors(date_string):
+                            if time_flag_seconds(date_string, 10):
                                 chrono_trigger = True
                                 break
                     if not chrono_trigger:
